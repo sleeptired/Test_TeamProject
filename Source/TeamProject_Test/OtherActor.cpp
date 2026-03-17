612f131be3a6fc1a382164880157aaf2c1bee2a5
sleeptired <107916334+sleeptired@users.noreply.github.com>
2026-03-17 19:51:47 +0900

    Local branch Create
    main Local branch Save point로 쓰기위해 작업내용을 여기에 따로 파서 진행

--- Source/TeamProject_Test/OtherActor.cpp
+++ Source/TeamProject_Test/OtherActor.cpp
@@ -14,12 +14,16 @@ AOtherActor::AOtherActor()
 void AOtherActor::BeginPlay()
 {
 	Super::BeginPlay();
+
+	UE_LOG(LogTemp, Warning, TEXT("AOtherActor has begun play!"));
 	
 }
 
 void AOtherActor::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
+
+	UE_LOG(LogTemp, Warning, TEXT("AOhterActor is ticking!"));
 
 }
 
